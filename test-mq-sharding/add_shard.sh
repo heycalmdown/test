9859611b9b28679c847480ce667d4070b96172a0
heycalmdown <heyacct@gmail.com>
2018-03-31 18:39:36 +0900

    mq-sharding: init + add_shard

--- test-mq-sharding/add_shard.sh
+++
@@ -1,4 +0,0 @@
-docker-compose exec mq1 rabbitmqctl set_policy test-shard ^test$ '{"shards-per-node":4}'
-curl -i -u guest:guest -H "content-type:application/json" \
-    -XPUT -d'{"type":"x-modulus-hash","durable":false}' \
-    http://localhost:15672/api/exchanges/%2f/test
--- test-mq-sharding/init.sh
+++ test-mq-sharding/init.sh
@@ -2,3 +2,7 @@ docker-compose exec mq1 rabbitmq-plugins enable rabbitmq_sharding
 docker-compose exec mq2 rabbitmqctl stop_app
 docker-compose exec mq2 rabbitmqctl join_cluster rabbit@mq1
 docker-compose exec mq2 rabbitmqctl start_app
+docker-compose exec mq1 rabbitmqctl set_policy test-shard ^test$ '{"shards-per-node":4}'
+curl -i -u guest:guest -H "content-type:application/json" \
+    -XPUT -d'{"type":"x-modulus-hash","durable":false}' \
+    http://localhost:15672/api/exchanges/%2f/test
